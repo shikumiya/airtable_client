b49c5e0776cd0eb8f4d114cecdccaea33d1bfe6c
Yuji Hata <hata@shikumiya.co.jp>
2020-09-18 20:26:40 +0900

    testing.

---
+++ airtable/__version__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__name__ = "airtable_client"
+__description__ = "Python API Wrapper for the Airtable API"
+__url__ = "https://github.com/shikumiya/airtable_client"
+__author__ = "shikumiya"
+__authoremail__ = "info@shikumiya.co.jp"
+__license__ = "The MIT License (MIT)"
+__copyright__ = "Copyright 2020 Shikumiya, Inc."
